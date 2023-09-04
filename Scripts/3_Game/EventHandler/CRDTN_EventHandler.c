f9d2a113a663f95f1fdefc8161507ae3787b538e
Freeman <freeman1992@gmail.com>
2023-09-05 01:57:01 +0200

    [Change]: Event Handler

--- Scripts/3_Game/EventHandler/CRDTN_EventHandler.c
+++ Scripts/3_Game/EventHandler/CRDTN_EventHandler.c
@@ -5,24 +5,28 @@ class CRDTN_EventHandler
 	void CRDTN_EventHandler()
 	{
 		events = new map<string, ref ScriptInvoker>;
-		AddEvent(CRDTN_EVT_PLAYER_FACTION_SYNC);
 	}
+
 	void AddEvent(string eventName)
 	{
 		if(events.Contains(eventName))
 		{
-			Print("[" + ClassName() + "]:: Duplicate Event Added: " + eventName);
+			DebugUtils.Log(CFG_CRDTN_Core_Prefix + " CRDTN_EventHandler :: Event already exists: " + eventName);
 			return;
 		}
 		if(!events.Contains(eventName))
+		{
+			DebugUtils.Log(CFG_CRDTN_Core_Prefix + " CRDTN_EventHandler :: AddEvent() - " + eventName);
 			events.Insert(eventName, new ScriptInvoker());
+		}
 	}
+
 	ScriptInvoker GetEventInvoker(string eventName)
 	{
 		ScriptInvoker eventInvoker;
 		if(!events.Contains(eventName))
 		{
-			Print("[" + ClassName() + "]:: Invalid Event: " + eventName);
+			DebugUtils.Log(CFG_CRDTN_Core_Prefix + " CRDTN_EventHandler :: Invalid Event: " + eventName);
 			return null;
 		}
 		if(events.Contains(eventName))
@@ -30,5 +34,20 @@ class CRDTN_EventHandler
 			eventInvoker = events[eventName];
 		}
 		return eventInvoker;
+	}
+
+	void RemoveEvent(string eventName)
+	{
+		if(!events.Contains(eventName))
+		{
+			DebugUtils.Log(CFG_CRDTN_Core_Prefix + " CRDTN_EventHandler :: Invalid Event: " + eventName);
+			return;
+		}
+		
+		if(events.Contains(eventName))
+		{
+			DebugUtils.Log(CFG_CRDTN_Core_Prefix + " CRDTN_EventHandler :: RemoveEvent() - " + eventName);
+			events.Remove(eventName);
+		}
 	}
 };
