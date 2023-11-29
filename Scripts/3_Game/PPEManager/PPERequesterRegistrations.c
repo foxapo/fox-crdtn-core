f39cbaeb3011e324d240fc6c8c756170705d696e
Freeman <freeman1992@gmail.com>
2023-11-29 15:12:22 +0100

    [Fix]: sVisual experimental

--- Scripts/3_Game/PPEManager/PPERequesterRegistrations.c
+++ Scripts/3_Game/PPEManager/PPERequesterRegistrations.c
@@ -3,5 +3,9 @@ modded class PPERequesterRegistrations
     void PPERequesterRegistrations()
     {
         PPERequesterBank.RegisterRequester(CRDTN_PPERequester_Player);
+#ifdef S_VISUAL
+
+        PPERequesterBank.RegisterRequester(CRDTN_SV_PsiEffect);
+#endif
     }
 }
